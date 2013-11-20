181596fe81c66918e904116985863293ff8ac5b8
Pramod <pramodv_nyros@yahoo.com>
2013-11-20 19:18:50 +0530

    commit entire app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviceCanAdmin::Application.config.secret_token = 'c4ccee4e2a91cf58991f9bd634d676991063885e10fe07dcd5a8fbec964d44a4da873b1d4bb057037bac444eafdc94e98deb6614615fcb8f678a78cbce446392'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DeviceCanAdmin::Application.config.session_store :cookie_store, key: '_device_can_admin_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DeviceCanAdmin::Application.config.session_store :active_record_store
